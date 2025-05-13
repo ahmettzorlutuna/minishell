fa75c387064f96ab548b9676890a8b96998ca554
Ahmet <ahmettayyip3439@gmail.com>
2025-05-14 00:04:38 +0300

    pwd - readline ve stat fonksiyonun test'i

--- main/main.c
+++ main/main.c
@@ -10,9 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../include/minishell.h"
 
-int main() {
-    printf("Tesstte");
+void check_type(const char *path)
+{
+    struct stat sb;
+    if(stat(path, &sb) == -1)
+    {
+        perror("stat");
+        return;
+    }
+    else
+    {
+        if(S_ISDIR(sb.st_mode))
+            printf("%s bu bir klasör\n", path);
+        else if(S_ISREG(sb.st_mode))
+            printf("%s bu bir dosya\n", path);
+        else if(S_ISLNK(sb.st_mode))
+            printf("%s bu bir link\n", path);
+    }
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    (void)argc;
+    (void)argv;
+
+    char *rl;
+    char *pwd;
+    check_type("/home/parallels/Desktop/minishell/main/file.txt");
+    check_type("/home/parallels/Desktop/minishell/main");
+    while(1)
+    {
+        rl = readline("bash$ ");
+        pwd = getcwd(NULL, 0);
+        if (pwd == NULL)
+        {
+            perror("getcwd");
+            free(rl);
+            continue;
+        }
+        if (rl == NULL)
+        {
+            printf("exit\n");
+            break;
+        }
+        if (*rl)
+            add_history(rl);
+        printf("Before chdir the path is : %s\n", pwd);
+        chdir("/home/parallels/Desktop");
+        pwd = getcwd(NULL, 0);
+        check_type(pwd);
+        printf("Input: %s\n", rl);
+        free(rl);
+    }
+    
     return 0;
 }

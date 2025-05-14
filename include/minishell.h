5937892a0953fd646dfe9fca6e603b9345d7d056
Ahmet <ahmettayyip3439@gmail.com>
2025-05-15 00:58:36 +0300

    command ve redirection structları

--- include/minishell.h
+++ include/minishell.h
@@ -27,20 +27,46 @@
 #include <curses.h> // for tgetent(), tgetstr(), tputs()
 #include <term.h> // for tgetent(), tgetstr(), tputs()
 
+//* ************************************************************************** */
+//* Structures
+//* ************************************************************************** */
+
 typedef struct s_minishell
 {
-    char **envp;
-    char *input;
-    char *prompt;
+    char **envp; // Çevresel değişkenler
+    char *input; // Kullanıcıdan alınan girdi
+	int last_exit_code; // Son çıkış kodu $?
 } t_minishell;
 
 typedef struct s_token
 {
-	t_token_type	type;
-	char			*value;
-	struct s_token	*next;
-    struct s_token	*prev;
+	t_token_type	type; // Token türü (örneğin: TOKEN_WORD, TOKEN_REDIRECT_IN, vb.)
+	char			*value; // Tokenin değeri (örneğin: "hello", ">", "<", "|", vb.)
+	struct s_token	*next; // Sonraki tokeni işaret eder
+    struct s_token	*prev; // Önceki tokeni işaret eder
 }	t_token;
+
+typedef struct s_redirection
+{
+    t_token_type type;  // Yönlendirme türü (in, out, append, vb.)
+    char *file;         // Yönlendirilen dosya adı
+    struct s_redirection *next; // Birden fazla yönlendirme olabilir
+} t_redirection;
+
+typedef struct s_command
+{
+    char *cmd;             // Komut adı (örneğin: echo, pwd, cd, vb.)
+    char **args;           // Komutun argümanları (örneğin: ["-n", "Hello"])
+    t_redirection *redir;  // Yönlendirmeler (input, output, append, vb.)
+    struct s_command *next; // Birden fazla komut için linked list
+} t_command;
+
+typedef struct s_env
+{
+	char	*key;   // Örn: "PATH"
+	char	*value; // Örn: "/usr/bin:/bin"
+	struct s_env *next;
+}	t_env;
 
 
 //* ************************************************************************** */
@@ -48,14 +74,14 @@ typedef struct s_token
 //* ************************************************************************** */
 typedef enum e_token_type
 {
-	T_WORD,         // ls, -la, txt gibi normal kelimeler
-    T_VAR,          // $PATH, $USER gibi değişkenler
-	T_PIPE,         // |
-	T_REDIRECT_IN,  // <
-	T_REDIRECT_OUT, // >
-	T_HEREDOC,      // <<
-	T_APPEND,       // >>
-	T_EOF           // Son token (listeyi sonlandırmak için)
+	TOKEN_WORD,         // ahmet, foo, "merhaba"
+    TOKEN_CMD,          // Komutlar (örneğin: echo, cd, pwd)
+	TOKEN_REDIRECT_IN,  // <
+	TOKEN_REDIRECT_OUT, // >
+	TOKEN_HEREDOC,      // <<
+	TOKEN_APPEND,       // >>
+	TOKEN_PIPE,         // |
+	TOKEN_EOF           // Son token (listeyi sonlandırmak için)
 }	t_token_type;
 
 typedef enum e_quote_type

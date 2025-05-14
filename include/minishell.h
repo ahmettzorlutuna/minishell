7fc622297d6ae5ed37a2b60eda01b8047f6b2c93
Ahmet <ahmettayyip3439@gmail.com>
2025-05-15 00:35:14 +0300

    enumlar ve iki adet token ve minishell struct'ı oluşturuldu.

--- include/minishell.h
+++ include/minishell.h
@@ -22,5 +22,47 @@
 # include <dirent.h> //opendir(), readdir(), closedir()
 # include <string.h> //strerror()
 # include <fcntl.h> //open(), O_RDONLY
+# include <sys/ioctl.h> //ioctl()
+
+#include <curses.h> // for tgetent(), tgetstr(), tputs()
+#include <term.h> // for tgetent(), tgetstr(), tputs()
+
+typedef struct s_minishell
+{
+    char **envp;
+    char *input;
+    char *prompt;
+} t_minishell;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+    struct s_token	*prev;
+}	t_token;
+
+
+//* ************************************************************************** */
+//* Enums
+//* ************************************************************************** */
+typedef enum e_token_type
+{
+	T_WORD,         // ls, -la, txt gibi normal kelimeler
+    T_VAR,          // $PATH, $USER gibi değişkenler
+	T_PIPE,         // |
+	T_REDIRECT_IN,  // <
+	T_REDIRECT_OUT, // >
+	T_HEREDOC,      // <<
+	T_APPEND,       // >>
+	T_EOF           // Son token (listeyi sonlandırmak için)
+}	t_token_type;
+
+typedef enum e_quote_type
+{
+    NO_QUOTE,
+    SINGLE_QUOTE,
+    DOUBLE_QUOTE
+}	t_quote_type;
 
 #endif

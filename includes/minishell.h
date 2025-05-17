b5de4f74f78c0b5cfcd104f62afe6a49e2d2c288
Ahmet Tayyip Zorlutuna <azorlutu@k1m35s02.42istanbul.com.tr>
2025-05-17 23:36:16 +0300

    norm için indent yaptım

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,44 +11,47 @@
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include <stdio.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <sys/stat.h> //stat(), fstat(), lstat()
-# include <dirent.h> //opendir(), readdir(), closedir()
-# include <string.h> //strerror()
-# include <fcntl.h> //open(), O_RDONLY
-# include <sys/ioctl.h> //ioctl()
+#include <stdio.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>  //stat(), fstat(), lstat()
+#include <dirent.h>	   //opendir(), readdir(), closedir()
+#include <string.h>	   //strerror()
+#include <fcntl.h>	   //open(), O_RDONLY
+#include <sys/ioctl.h> //ioctl()
+#include <signal.h>	   //signal()
 
 #include <curses.h> // for tgetent(), tgetstr(), tputs()
-#include <term.h> // for tgetent(), tgetstr(), tputs()
+#include <term.h>	// for tgetent(), tgetstr(), tputs()
 
 #include "../libft/libft.h"
+
+extern volatile sig_atomic_t g_signal_flag;
 
 //* ************************************************************************** */
 //* Enums
 //* ************************************************************************** */
 typedef enum e_token_type
 {
-	TOKEN_WORD,         // Kelime (örneğin: "hello", "world")
-	TOKEN_REDIRECT_IN,  // <
+	TOKEN_WORD,			// Kelime (örneğin: "hello", "world")
+	TOKEN_REDIRECT_IN,	// <
 	TOKEN_REDIRECT_OUT, // >
-	TOKEN_HEREDOC,      // <<
-	TOKEN_APPEND,       // >>
-	TOKEN_PIPE,         // |
-	TOKEN_EOF           // Son token (listeyi sonlandırmak için)
-}	t_token_type;
+	TOKEN_HEREDOC,		// <<
+	TOKEN_APPEND,		// >>
+	TOKEN_PIPE,			// |
+	TOKEN_EOF			// Son token (listeyi sonlandırmak için)
+} t_token_type;
 
 typedef enum e_quote_type
 {
-    NO_QUOTE,
-    SINGLE_QUOTE,
-    DOUBLE_QUOTE
-}	t_quote_type;
+	NO_QUOTE,
+	SINGLE_QUOTE,
+	DOUBLE_QUOTE
+} t_quote_type;
 
 //* ************************************************************************** */
 //* Structures
@@ -56,43 +59,45 @@ typedef enum e_quote_type
 
 typedef struct s_token
 {
-	t_token_type	type; // Token türü (örneğin: TOKEN_WORD, TOKEN_REDIRECT_IN, vb.)
-	char			*value; // Tokenin değeri (örneğin: "hello", ">", "<", "|", vb.)
-	struct s_token	*next; // Sonraki tokeni işaret eder
-}	t_token;
+	t_token_type type;	  // Token türü (örneğin: TOKEN_WORD, TOKEN_REDIRECT_IN, vb.)
+	char *value;		  // Tokenin değeri (örneğin: "hello", ">", "<", "|", vb.)
+	struct s_token *next; // Sonraki tokeni işaret eder
+} t_token;
 
 typedef struct s_redir
 {
-    t_token_type type;  // Yönlendirme türü (in, out, append, vb.)
-    char *file;         // Yönlendirilen dosya adı
-    struct s_redir *next; // Birden fazla yönlendirme olabilir
+	t_token_type type;	  // Yönlendirme türü (in, out, append, vb.)
+	char *file;			  // Yönlendirilen dosya adı
+	struct s_redir *next; // Birden fazla yönlendirme olabilir
 } t_redirection;
 
 typedef struct s_cmd
 {
-    char *cmd;             // Komut adı
-    char **args;           // Komutun argümanları (null-terminated)
-    t_redirection *redir;  // Yönlendirmeler
-    char *heredoc_content; // Heredoc içeriği (veya geçici dosya adı)
-    char *heredoc_delimiter; // Heredoc sınırlayıcısı
-    int pipe_in_fd;       // Önceki komuttan okuma için pipe FD
-    int pipe_out_fd;      // Sonraki komuta yazma için pipe FD
-    struct s_cmd *next;   // Boru hattındaki sonraki komut
+	char *cmd;				 // Komut adı
+	char **args;			 // Komutun argümanları (null-terminated)
+	t_redirection *redir;	 // Yönlendirmeler
+	char *heredoc_content;	 // Heredoc içeriği (veya geçici dosya adı)
+	char *heredoc_delimiter; // Heredoc sınırlayıcısı
+	int pipe_in_fd;			 // Önceki komuttan okuma için pipe FD
+	int pipe_out_fd;		 // Sonraki komuta yazma için pipe FD
+	struct s_cmd *next;		 // Boru hattındaki sonraki komut
 } t_command;
 
 typedef struct s_env
 {
-	char	*key;   // Örn: "PATH"
-	char	*value; // Örn: "/usr/bin:/bin"
+	char *key;	 // Örn: "PATH"
+	char *value; // Örn: "/usr/bin:/bin"
 	struct s_env *next;
-}	t_env;
+} t_env;
 
 typedef struct s_minishell
 {
-    char *input; // Kullanıcıdan alınan girdi
-    t_env *env_list; // Çevresel değişkenler
-	int last_exit_code; // Son çıkış kodu $?
-    int number_of_prompts; // Kaç tane prompt gösterildi
+	char *input;		   // Kullanıcıdan alınan girdi
+	t_env *env_list;	   // Çevresel değişkenler
+	int last_exit_code;	   // Son çıkış kodu $?
+	int number_of_prompts; // Kaç tane prompt gösterildi
 } t_minishell;
+
+void init_signal_handler(void);
 
 #endif

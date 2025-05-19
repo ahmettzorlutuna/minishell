31c0ec16f26d4af17e3b541e4a997c5b76c1b089
Ahmet <ahmettayyip3439@gmail.com>
2025-05-20 00:09:55 +0300

    tokenların linked list implementasyonu

---
+++ srcs/lexer/token_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azorlutu <azorlutu@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 22:18:05 by azorlutu          #+#    #+#             */
+/*   Updated: 2025/05/19 22:18:05 by azorlutu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void free_token_list(t_token *head)
+{
+	t_token *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		if (tmp->value)
+			free(tmp->value);
+		free(tmp);
+	}
+}
+
+void add_token(t_token **head, t_token *new_token)
+{
+    t_token *current;
+
+    if(!head || !new_token)
+        return ;
+    if(!*head)
+    {
+        *head = new_token;
+        return ;
+    }
+    current = *head;
+    while (current->next)
+        current = current->next;
+    current->next = new_token;
+}
+
+t_token	*create_token(t_token_type type, char *value)
+{
+    t_token	*new_token;
+
+    new_token = (t_token *)malloc(sizeof(t_token));
+    if (!new_token)
+        return (NULL);
+    new_token->type = type;
+    new_token->value = value;
+    new_token->next = NULL;
+    return (new_token);
+}

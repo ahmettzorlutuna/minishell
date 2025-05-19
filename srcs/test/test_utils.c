33c88256be757a20a57d1938295247048602c14f
Ahmet <ahmettayyip3439@gmail.com>
2025-05-20 00:08:52 +0300

    tokenları bastıracağım fonksiyon

---
+++ srcs/test/test_utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azorlutu <azorlutu@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 23:49:30 by azorlutu          #+#    #+#             */
+/*   Updated: 2025/05/19 23:49:31 by azorlutu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static const char *token_type_str(t_token_type type)
+{
+	if (type == TOKEN_WORD)
+		return "WORD";
+	else if (type == TOKEN_REDIRECT_IN)
+		return "REDIRECT_IN (<)";
+	else if (type == TOKEN_REDIRECT_OUT)
+		return "REDIRECT_OUT (>)";
+	else if (type == TOKEN_HEREDOC)
+		return "HEREDOC (<<)";
+	else if (type == TOKEN_APPEND)
+		return "APPEND (>>)";
+	else if (type == TOKEN_PIPE)
+		return "PIPE (|)";
+	else if (type == TOKEN_EOF)
+		return "EOF";
+	return "UNKNOWN";
+}
+
+void print_tokens(t_token *tokens)
+{
+	while (tokens)
+	{
+		printf("Token Type: %-16s | Value: %s\n",
+			token_type_str(tokens->type),
+			tokens->value ? tokens->value : "NULL");
+		tokens = tokens->next;
+	}
+}

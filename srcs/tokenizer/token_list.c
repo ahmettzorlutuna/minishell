e720290281411c5e2485f6f4d31045b10fda7c25
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2025-05-23 23:58:01 +0300

    token oluşturur ve listeye ekler

---
+++ srcs/tokenizer/token_list.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_list.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azorlutu <azorlutu@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 22:18:05 by azorlutu          #+#    #+#             */
+/*   Updated: 2025/05/19 22:18:05 by azorlutu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void add_token(t_token **head, t_token *new_token)
+{
+    t_token *current;
+
+    if(!head || !new_token)
+        return ;
+    if(!*head)
+    {
+        *head = new_token;
+        return ;
+    }
+    current = *head;
+    while (current->next)
+        current = current->next;
+    current->next = new_token;
+}
+
+t_token	*create_token(t_token_type type, char *value)
+{
+    t_token	*new_token;
+
+    new_token = (t_token *)malloc(sizeof(t_token));
+    if (!new_token)
+        return (NULL);
+    new_token->type = type;
+    new_token->value = value;
+    new_token->next = NULL;
+    return (new_token);
+}

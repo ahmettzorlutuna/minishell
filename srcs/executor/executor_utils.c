6dca783849c1b0320b1bb43dee6599637c6de221
Ahmet Tayyip Zorlutuna <azorlutu@k1m35s02.42istanbul.com.tr>
2025-05-30 21:12:31 +0300

    utils falan

--- srcs/executor/executor_utils.c
+++ srcs/executor/executor_utils.c
@@ -12,6 +12,40 @@
 
 #include "../includes/minishell.h"
 
+char *resolve_path(const char *cmd, t_env *env_list)
+{
+    char	**paths;
+    char	*path_value;
+    char	*temp_path;
+    int		i;
+
+	if(!cmd || !*cmd)
+		return (NULL);
+	if(ft_strchr(cmd, '/') && is_path_executable(cmd))
+		return (ft_strdup(cmd));
+
+	path_value = get_env_value(env_list, "PATH");
+	if(!path_value)
+		return (NULL);
+	paths = ft_split(path_value, ':');
+	if(!paths)
+		return (NULL);
+	i = 0;
+	while (paths[i])
+	{
+		temp_path = ft_strjoin_three(paths[i], "/", cmd);
+		if(is_path_executable(temp_path))
+		{
+			free_split(paths);
+			return (temp_path);
+		}
+		free(temp_path);
+		i++;
+	}
+	free_split(paths);
+	return (NULL);
+}
+
 void free_split(char **split)
 {
     int i;
@@ -41,5 +75,10 @@ char *ft_strjoin_three(const char *s1, const char *s2, const char *s3)
     full = ft_strjoin(first, s3);
     free(first);
     return (full);
+}
+
+int is_path_executable(const char *path)
+{
+	return (access(path, X_OK) == 0);
 }
 

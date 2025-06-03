802fa49ab55384e24fa6943ee48baf667b9a65d5
Ahmet <ahmettayyip3439@gmail.com>
2025-06-03 23:55:54 +0300

    builtin utils yani bu kadar

--- srcs/builtins/builtin_utils.c
+++ srcs/builtins/builtin_utils.c
@@ -12,36 +12,42 @@
 
 #include "../includes/minishell.h"
 
-int	is_builtin(char *cmd)
-{
-    if (!cmd)
-        return (0);
-    return (!ft_strcmp(cmd, "echo")
-        || !ft_strcmp(cmd, "cd")
-        || !ft_strcmp(cmd, "pwd")
-        || !ft_strcmp(cmd, "export")
-        || !ft_strcmp(cmd, "unset")
-        || !ft_strcmp(cmd, "env")
-        || !ft_strcmp(cmd, "exit"));
-}
-
 int	run_builtin(t_command *cmd, t_minishell *minishell)
 {
-    if (!cmd || !cmd->args || !cmd->args[0])
-        return (1);
-    if (!ft_strcmp(cmd->args[0], "echo"))
-        return (builtin_echo(cmd->args));
-    else if (!ft_strcmp(cmd->args[0], "cd"))
-        return (builtin_cd(cmd->args, minishell));
-    else if (!ft_strcmp(cmd->args[0], "pwd"))
-        return (builtin_pwd());
-    else if (!ft_strcmp(cmd->args[0], "export"))
-        return (builtin_export(cmd->args, minishell));
-    else if (!ft_strcmp(cmd->args[0], "unset"))
-        return (builtin_unset(cmd->args, minishell));
-    else if (!ft_strcmp(cmd->args[0], "env"))
-        return (builtin_env(minishell));
-    else if (!ft_strcmp(cmd->args[0], "exit"))
-        return (builtin_exit(cmd->args, minishell));
-    return (1);
+    (void)minishell;
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (1);
+	if (!ft_strcmp(cmd->args[0], "echo"))
+		return (builtin_echo(cmd->args));
+	else if (!ft_strcmp(cmd->args[0], "cd"))
+		return (builtin_cd(cmd->args, minishell));
+	else if (!ft_strcmp(cmd->args[0], "pwd"))
+		return (builtin_pwd());
+	else if (!ft_strcmp(cmd->args[0], "export"))
+		return (builtin_export(cmd->args, minishell));
+	// else if (!ft_strcmp(cmd->args[0], "unset"))
+	// 	return (builtin_unset(cmd->args, minishell));
+	// else if (!ft_strcmp(cmd->args[0], "env"))
+	// 	return (builtin_env(minishell));
+	// else if (!ft_strcmp(cmd->args[0], "exit"))
+	// 	return (builtin_exit(cmd->args, minishell));
+	return (1);
+}
+
+int	is_parent_builtin(char *cmd)
+{
+	if (!cmd)
+		return (0);
+	return (!ft_strcmp(cmd, "cd")
+		|| !ft_strcmp(cmd, "export")
+		|| !ft_strcmp(cmd, "unset")
+		|| !ft_strcmp(cmd, "exit"));
+}
+
+int execute_single_builtin(t_command *cmd, t_minishell *minishell)
+{
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (1);
+	minishell->last_exit_code = run_builtin(cmd, minishell);
+	return (0);
 }

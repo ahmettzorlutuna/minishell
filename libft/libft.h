99007df8ea98f6721956e2d3bc02f18cd9cbc144
Ahmet <ahmettayyip3439@gmail.com>
2025-06-04 00:07:33 +0300

    ft_strndup added

--- libft/libft.h
+++ libft/libft.h
@@ -60,6 +60,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strndup(const char *s, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strcpy(char *dest, const char *src);

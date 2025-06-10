4b11c42ee78b50ea57fcfe105b883f4763b7d1b9
Ahmet <ahmettayyip3439@gmail.com>
2025-06-10 21:14:20 +0300

    builtin env

--- srcs/builtins/builtin_env.c
+++ srcs/builtins/builtin_env.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
 
+int	builtin_env(char **args, t_minishell *minishell)
+{
+    printf("girdi");
+	t_env	*tmp;
+
+	if (args[1])
+	{
+		ft_putstr_fd("env: too many arguments\n", 2);
+		return (127);
+	}
+	tmp = minishell->env_list;
+	while (tmp)
+	{
+		if (tmp->value)
+		{
+			ft_putstr_fd(tmp->key, 1);
+			ft_putchar_fd('=', 1);
+			ft_putstr_fd(tmp->value, 1);
+			ft_putchar_fd('\n', 1);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}

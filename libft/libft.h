75bd3a0ecf1a054719d685dd61066691aa4ede35
Ahmet <ahmettayyip3439@gmail.com>
2025-06-10 21:14:56 +0300

    ft_atol added

--- libft/libft.h
+++ libft/libft.h
@@ -38,6 +38,8 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
 
+long	ft_atol(const char *str);
+
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);

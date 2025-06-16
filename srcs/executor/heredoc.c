b059be75d57a0c37f390b38f1df106cc0d3cb1c8
Ahmet <ahmettayyip3439@gmail.com>
2025-06-16 22:02:20 +0300

    yeni sinyal fonksiyonu ama heredoc sorunlu şu an

--- srcs/executor/heredoc.c
+++ srcs/executor/heredoc.c
@@ -18,6 +18,7 @@ int handle_heredoc(t_command *cmd)
     int pipefd[2];
     char *line;
 
+	g_signal_flag = 0; // Reset signal flag for heredoc
     while (cmd)
     {
         redir = cmd->redirects;
@@ -27,17 +28,30 @@ int handle_heredoc(t_command *cmd)
             {
                 if(pipe(pipefd) == -1)
                     return (1);
+				setup_heredoc_signals();
                 while (1)
                 {
                     line = readline("> ");
+					if (!line)
+					{
+						if (g_signal_flag == SIGINT)
+						{
+							close(pipefd[0]);
+							close(pipefd[1]);
+							setup_interactive_signals();
+							return (1); // heredoc iptal
+						}
+						break;
+					}
                     if(!line || !redir->filename || ft_strcmp(line, redir->filename) == 0)
                         break;
                     write(pipefd[1], line, ft_strlen(line));
                     write(pipefd[1], "\n", 1);
                     free(line);
                 }
+				setup_interactive_signals();
                 if(line)
-                    free(line);
+					free(line);
                 close(pipefd[1]);
                 redir->fd = pipefd[0];
             }

d5e0940b96e72340968835d71c5439454e1c7f94
Ahmet Tayyip Zorlutuna <azorlutu@k1m35s02.42istanbul.com.tr>
2025-06-22 16:58:38 +0300

    heredoc'u childda çalıştırdım. CTRL+C artık çıkış yapıyor.

--- srcs/executor/heredoc.c
+++ srcs/executor/heredoc.c
@@ -15,17 +15,23 @@
 int run_heredoc_child(const char *delimiter, int write_fd)
 {
 	char *line;
-
-	setup_heredoc_signals();
+	char *buffer;
 	g_signal_flag = 0;
 
+	buffer = ft_strdup("");
+	if(!buffer)
+		exit(1);
+	setup_heredoc_signals();
 	while (1)
 	{
 		line = readline("> ");
 		if (!line)
 		{
 			if (g_signal_flag == SIGINT) //ctrl + D
+			{
+				free(buffer);
 				exit(1);
+			}
 			break;
 		}
 		if (!delimiter || ft_strcmp(line, delimiter) == 0)
@@ -33,10 +39,15 @@ int run_heredoc_child(const char *delimiter, int write_fd)
 			free(line);
 			break;
 		}
-		write(write_fd, line, ft_strlen(line));
-		write(write_fd, "\n", 1);
+		buffer = ft_strjoin(buffer, line);
+		buffer = ft_strjoin(buffer, "\n");
 		free(line);
 	}
+
+	if(g_signal_flag != SIGINT)
+		write(write_fd, buffer,	ft_strlen(buffer));
+
+	free(buffer);
 	close(write_fd);
 	exit(0);
 }
@@ -63,14 +74,14 @@ int handle_heredoc(t_command *cmd)
 					return (1);
 				if (pid == 0)
 					run_heredoc_child(redir->filename, pipefd[1]);
-				close(pipefd[1]); // Child yazacak, parent sadece okuyacak
+				close(pipefd[1]);
 				waitpid(pid, &status, 0);
 				if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
 				{
 					write(1, "\n", 1);
 					close(pipefd[0]);
 					setup_interactive_signals();
-					return (1); // heredoc iptal edildi
+					return (1);
 				}
 				redir->fd = pipefd[0];
 			}

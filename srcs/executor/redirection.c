e3492f41584669af320524446bc39f55c5916694
Ahmet Tayyip Zorlutuna <azorlutu@k1m34s02.42istanbul.com.tr>
2025-07-01 23:11:38 +0300

    Norm OK

--- srcs/executor/redirection.c
+++ srcs/executor/redirection.c
@@ -40,39 +40,53 @@ int	create_empty_redirect_files(t_redirection *redir)
 	return (0);
 }
 
+static int	open_redirection(t_redirection *redir)
+{
+	int	fd;
+
+	if (redir->type == TOKEN_REDIRECT_IN)
+		fd = open(redir->filename, O_RDONLY);
+	else if (redir->type == TOKEN_REDIRECT_OUT)
+		fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (redir->type == TOKEN_APPEND)
+		fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (redir->type == TOKEN_HEREDOC)
+		fd = redir->fd;
+	else
+		return (-2);
+	return (fd);
+}
+
+static void	redirect_fd(t_redirection *redir, int fd)
+{
+	if (redir->type == TOKEN_REDIRECT_IN || redir->type == TOKEN_HEREDOC)
+	{
+		dup2(fd, STDIN_FILENO);
+		if (redir->type != TOKEN_HEREDOC)
+			close(fd);
+	}
+	else
+	{
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+}
+
 int	set_redirection_fds(t_redirection *redir)
 {
 	int	fd;
 
 	while (redir)
 	{
-		if (redir->type == TOKEN_REDIRECT_IN)
-			fd = open(redir->filename, O_RDONLY);
-		else if (redir->type == TOKEN_REDIRECT_OUT)
-			fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		else if (redir->type == TOKEN_APPEND)
-			fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else if (redir->type == TOKEN_HEREDOC)
-			fd = redir->fd;
-		else
+		fd = open_redirection(redir);
+		if (fd == -2)
 			return (1);
 		if (fd < 0)
 		{
 			perror(redir->filename);
 			return (1);
 		}
-		if (redir->type == TOKEN_REDIRECT_IN || redir->type == TOKEN_HEREDOC)
-		{
-			dup2(fd, STDIN_FILENO);
-			close(fd);
-		}
-		else
-		{
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
-		if (redir->type != TOKEN_HEREDOC)
-			close(fd);
+		redirect_fd(redir, fd);
 		redir = redir->next;
 	}
 	return (0);

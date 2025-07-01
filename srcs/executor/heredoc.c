0ec8c62b5f7a2722d86396b1f2cc6fd517a613af
Ahmet Tayyip Zorlutuna <azorlutu@k1m34s02.42istanbul.com.tr>
2025-07-01 23:11:46 +0300

    Norm OK

--- srcs/executor/heredoc.c
+++ srcs/executor/heredoc.c
@@ -12,47 +12,48 @@
 
 #include "../includes/minishell.h"
 
-int run_heredoc_child(const char *delimiter, int write_fd)
+static int	process_heredoc_result(t_minishell *minishell, int status, int fd)
 {
-	char *line;
-	char *buffer;
-	g_signal_flag = 0;
-
-	buffer = ft_strdup("");
-	if(!buffer)
-		exit(1);
-	while (1)
+	if (WIFEXITED(status))
 	{
-		line = readline("> ");
-		if (!line)
+		minishell->last_exit_code = WEXITSTATUS(status);
+		if (minishell->last_exit_code == 130)
 		{
-			free(buffer);
-			if (g_signal_flag == SIGINT)
-				exit(130);
-			exit(0);
+			close(fd);
+			setup_interactive_signals();
+			return (1);
 		}
-		if (!delimiter || ft_strcmp(line, delimiter) == 0)
-		{
-			free(line);
-			break;
-		}
-		buffer = ft_strjoin_free(buffer, line);
-		buffer = ft_strjoin_free(buffer, "\n");
 	}
-	write(write_fd, buffer, ft_strlen(buffer));
-	free(buffer);
-	close(write_fd);
-	exit(0);
+	else if (WIFSIGNALED(status))
+		minishell->last_exit_code = 128 + WTERMSIG(status);
+	return (0);
 }
 
-int handle_heredoc(t_command *cmd, t_minishell *minishell)
+static int	process_heredoc_pipe(t_redirection *redir, t_minishell *minishell)
 {
-	t_redirection *redir;
-	int pipefd[2];
-	pid_t pid;
-	int status;
+	int		pipefd[2];
+	pid_t	pid;
+	int		status;
 
-	(void)minishell;
+	if (pipe(pipefd) == -1)
+		return (1);
+	pid = fork();
+	if (pid < 0)
+		return (1);
+	if (pid == 0)
+		handle_heredoc_child(redir, pipefd);
+	close(pipefd[1]);
+	waitpid(pid, &status, 0);
+	if (process_heredoc_result(minishell, status, pipefd[0]))
+		return (1);
+	redir->fd = pipefd[0];
+	return (0);
+}
+
+int	handle_heredoc(t_command *cmd, t_minishell *minishell)
+{
+	t_redirection	*redir;
+
 	while (cmd)
 	{
 		redir = cmd->redirects;
@@ -60,33 +61,8 @@ int handle_heredoc(t_command *cmd, t_minishell *minishell)
 		{
 			if (redir->type == TOKEN_HEREDOC)
 			{
-				if (pipe(pipefd) == -1)
+				if (process_heredoc_pipe(redir, minishell))
 					return (1);
-				pid = fork();
-				if (pid < 0)
-					return (1);
-				if (pid == 0)
-				{
-					setup_heredoc_signals();
-					run_heredoc_child(redir->filename, pipefd[1]);
-				}
-				close(pipefd[1]);
-				waitpid(pid, &status, 0);
-				if(WIFEXITED(status))
-				{
-					minishell->last_exit_code = WEXITSTATUS(status);
-					if(minishell->last_exit_code == 130)
-					{
-						close(pipefd[0]);
-						setup_interactive_signals();
-						return (1);
-					}
-				}
-				else if(WIFSIGNALED(status))
-				{
-					minishell->last_exit_code = 128 + WTERMSIG(status);
-				}
-				redir->fd = pipefd[0];
 			}
 			redir = redir->next;
 		}

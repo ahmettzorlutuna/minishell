c7dbb65eedf3f1ab0f94e702e3f36cf0e385b500
Ahmet Tayyip Zorlutuna <azorlutu@k1m34s02.42istanbul.com.tr>
2025-07-01 23:10:47 +0300

    Norm OK

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -12,28 +12,6 @@
 
 #include "../includes/minishell.h"
 
-static	char	**list_to_array(t_list *args)
-{
-	int		count;
-	char	**result;
-	t_list	*tmp;
-	int		i;
-
-	count = ft_lstsize(args);
-	result = malloc(sizeof(char *) * (count + 1));
-	if (!result)
-		return (NULL);
-	tmp = args;
-	i = 0;
-	while (tmp)
-	{
-		result[i++] = ft_strdup(tmp->content);
-		tmp = tmp->next;
-	}
-	result[i] = NULL;
-	return (result);
-}
-
 static	int	parse_redirection(t_token *cursor, t_command *cmd)
 {
 	t_redirection	*redir;
@@ -54,19 +32,19 @@ static	int	parse_redirection(t_token *cursor, t_command *cmd)
 	return (0);
 }
 
-static	int	add_arg_to_list(t_list **args, char *value)
+static int	process_token(t_token **cursor, t_command *cmd, t_list **args)
 {
-	t_list	*new_node;
-
-	if (!value || !*value)
-		return (0);
-	new_node = ft_lstnew(ft_strdup(value));
-	if (!new_node)
+	if ((*cursor)->type == TOKEN_WORD)
+		add_arg_to_list(args, (*cursor)->value);
+	else if ((*cursor)->type == TOKEN_REDIRECT_IN
+		|| (*cursor)->type == TOKEN_REDIRECT_OUT
+		|| (*cursor)->type == TOKEN_HEREDOC
+		|| (*cursor)->type == TOKEN_APPEND)
 	{
-		perror("Error allocating memory for argument");
-		return (1);
+		if (parse_redirection(*cursor, cmd))
+			return (1);
+		*cursor = (*cursor)->next;
 	}
-	ft_lstadd_back(args, new_node);
 	return (0);
 }
 
@@ -76,34 +54,18 @@ t_command	*parse_command(t_token **tokens)
 	t_list		*args;
 	t_token		*cursor;
 
-	cmd = ft_calloc(1, sizeof(t_command));
+	cmd = init_command();
 	if (!cmd)
-	{
-		perror("Error allocating memory for command");
 		return (NULL);
-	}
 	args = NULL;
 	cursor = *tokens;
 	while (cursor && cursor->type != TOKEN_PIPE && cursor->type != TOKEN_EOF)
 	{
-		if (cursor->type == TOKEN_WORD)
-			add_arg_to_list(&args, cursor->value);
-		else if (cursor && (cursor->type == TOKEN_REDIRECT_IN
-				|| cursor->type == TOKEN_REDIRECT_OUT
-				|| cursor->type == TOKEN_HEREDOC
-				|| cursor->type == TOKEN_APPEND))
-		{
-			if (parse_redirection(cursor, cmd))
-				return (NULL);
-			cursor = cursor->next;
-		}
+		if (process_token(&cursor, cmd, &args))
+			return (NULL);
 		cursor = cursor->next;
 	}
-	if (cursor && cursor->type == TOKEN_PIPE)
-	{
-		cursor = cursor->next;
-		cmd->next_pipe = parse_command(&cursor);
-	}
+	handle_pipe_recursively(&cursor, cmd);
 	cmd->args = list_to_array(args);
 	ft_lstclear(&args, free);
 	return (cmd);

5c2c476c5f524d92301b779540b61aca1109f165
Ahmet Tayyip Zorlutuna <azorlutu@k1m34s02.42istanbul.com.tr>
2025-07-01 23:12:16 +0300

    Norm OK

--- srcs/executor/executor_utils.c
+++ srcs/executor/executor_utils.c
@@ -12,39 +12,6 @@
 
 #include "../includes/minishell.h"
 
-char	*resolve_path(const char *cmd, t_env *env_list)
-{
-	char	**paths;
-	char	*path_value;
-	char	*temp_path;
-	int		i;
-
-	if (!cmd || !*cmd)
-		return (NULL);
-	if (ft_strchr(cmd, '/'))
-		return (ft_strdup(cmd));
-	path_value = get_env_value(env_list, "PATH");
-	if (!path_value)
-		return (NULL);
-	paths = ft_split(path_value, ':');
-	if (!paths)
-		return (NULL);
-	i = 0;
-	while (paths[i])
-	{
-		temp_path = ft_strjoin_three(paths[i], "/", cmd);
-		if (access(temp_path, F_OK) == 0)
-		{
-			free_split(paths);
-			return (temp_path);
-		}
-		free(temp_path);
-		i++;
-	}
-	free_split(paths);
-	return (NULL);
-}
-
 void	free_split(char **split)
 {
 	int	i;
@@ -88,15 +55,11 @@ void	print_and_exit(char *prefix, char *msg, int code)
 	exit(code);
 }
 
-void	check_and_execute(t_command *cmd, t_minishell *minishell)
+static char	*get_executable_path(t_command *cmd,
+						t_minishell *minishell, struct stat *sb)
 {
-	char		*path;
-	struct stat	sb;
+	char	*path;
 
-	if (!cmd->args[0] || cmd->args[0][0] == '\0')
-		exit(0);
-	if (is_builtin(cmd->args[0]))
-		exit(run_builtin(cmd, minishell));
 	path = resolve_path(cmd->args[0], minishell->env_list);
 	if (!path || path[0] == '\0')
 	{
@@ -107,21 +70,33 @@ void	check_and_execute(t_command *cmd, t_minishell *minishell)
 		}
 		print_and_exit(cmd->args[0], "command not found", 127);
 	}
-	if (stat(path, &sb) != 0)
+	if (stat(path, sb) != 0)
 	{
 		if (errno == ENOENT)
 			print_and_exit(path, "No such file or directory", 127);
 		if (errno == EACCES)
 			print_and_exit(path, "Permission denied", 126);
 	}
-	if (S_ISDIR(sb.st_mode))
+	if (S_ISDIR(sb->st_mode))
 		print_and_exit(path, "Is a directory", 126);
 	if (access(path, X_OK) != 0)
 		print_and_exit(path, "Permission denied", 126);
+	return (path);
+}
+
+void	check_and_execute(t_command *cmd, t_minishell *minishell)
+{
+	char		*path;
+	struct stat	sb;
+
+	if (!cmd->args[0] || cmd->args[0][0] == '\0')
+		exit(0);
+	if (is_builtin(cmd->args[0]))
+		exit(run_builtin(cmd, minishell));
+	path = get_executable_path(cmd, minishell, &sb);
 	execve(path, cmd->args, minishell->env_array);
 	perror("minishell");
 	if (errno == EACCES)
 		exit(126);
-	else
-		exit(127);
+	exit(127);
 }

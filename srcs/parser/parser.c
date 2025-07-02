2a3931b91bc3376a80c55abe6aa580eab05941b8
Ahmet Tayyip Zorlutuna <azorlutu@k1m34s02.42istanbul.com.tr>
2025-07-03 00:52:10 +0300

    heredoc delimiter token'ı handle edildi. tırnaklı tırnaksız expand olayları tamam. leak yok

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -12,7 +12,36 @@
 
 #include "../includes/minishell.h"
 
-static	int	parse_redirection(t_token *cursor, t_command *cmd)
+static int	init_heredoc_redir(t_minishell *minishell,
+				t_redirection *redir, t_token *token)
+{
+	redir->delimiter_raw = ft_strdup(token->value);
+	if (!redir->delimiter_raw)
+		return (1);
+	redir->redir_quote = token->quote;
+	if (redir->redir_quote == NO_QUOTE)
+		redir->delimiter_expanded = expand_word(minishell,
+				NO_QUOTE, redir->delimiter_raw, minishell->env_list);
+	else
+		redir->delimiter_expanded = ft_strdup(redir->delimiter_raw);
+	if (!redir->delimiter_expanded)
+	{
+		free(redir->delimiter_raw);
+		return (1);
+	}
+	return (0);
+}
+
+static int	init_filename_redir(t_redirection *redir, t_token *token)
+{
+	redir->filename = ft_strdup(token->value);
+	if (!redir->filename)
+		return (1);
+	return (0);
+}
+
+static int	parse_redirection(t_minishell *minishell,
+				t_token *cursor, t_command *cmd)
 {
 	t_redirection	*redir;
 	t_redirection	**redir_ptr;
@@ -24,7 +53,13 @@ static	int	parse_redirection(t_token *cursor, t_command *cmd)
 		return (1);
 	redir->fd = -1;
 	redir->type = cursor->type;
-	redir->filename = ft_strdup(cursor->next->value);
+	if (cursor->type == TOKEN_HEREDOC)
+	{
+		if (init_heredoc_redir(minishell, redir, cursor->next))
+			return (free(redir), 1);
+	}
+	else if (init_filename_redir(redir, cursor->next))
+		return (free(redir), 1);
 	redir_ptr = &cmd->redirects;
 	while (*redir_ptr)
 		redir_ptr = &(*redir_ptr)->next;
@@ -32,7 +67,8 @@ static	int	parse_redirection(t_token *cursor, t_command *cmd)
 	return (0);
 }
 
-static int	process_token(t_token **cursor, t_command *cmd, t_list **args)
+static int	process_token(t_minishell *minishell,
+				t_token **cursor, t_command *cmd, t_list **args)
 {
 	if ((*cursor)->type == TOKEN_WORD)
 		add_arg_to_list(args, (*cursor)->value);
@@ -41,14 +77,14 @@ static int	process_token(t_token **cursor, t_command *cmd, t_list **args)
 		|| (*cursor)->type == TOKEN_HEREDOC
 		|| (*cursor)->type == TOKEN_APPEND)
 	{
-		if (parse_redirection(*cursor, cmd))
+		if (parse_redirection(minishell, *cursor, cmd))
 			return (1);
 		*cursor = (*cursor)->next;
 	}
 	return (0);
 }
 
-t_command	*parse_command(t_token **tokens)
+t_command	*parse_command(t_minishell *minishell, t_token **tokens)
 {
 	t_command	*cmd;
 	t_list		*args;
@@ -61,11 +97,11 @@ t_command	*parse_command(t_token **tokens)
 	cursor = *tokens;
 	while (cursor && cursor->type != TOKEN_PIPE && cursor->type != TOKEN_EOF)
 	{
-		if (process_token(&cursor, cmd, &args))
+		if (process_token(minishell, &cursor, cmd, &args))
 			return (NULL);
 		cursor = cursor->next;
 	}
-	handle_pipe_recursively(&cursor, cmd);
+	handle_pipe_recursively(minishell, &cursor, cmd);
 	cmd->args = list_to_array(args);
 	ft_lstclear(&args, free);
 	return (cmd);

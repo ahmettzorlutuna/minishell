63bcce1012d743fe5fd11799256277a2da966925
Ahmet Tayyip Zorlutuna <azorlutu@k1m34s02.42istanbul.com.tr>
2025-07-20 22:19:24 +0300

    executor öncesi syntax errorları kontrol ettim

--- srcs/executor/executor_utils2.c
+++ srcs/executor/executor_utils2.c
@@ -18,11 +18,44 @@ int	empty_or_null_command(t_command *cmd, t_minishell *minishell)
 	{
 		if (cmd && cmd->redirects)
 		{
+			t_redirection *redir = cmd->redirects;
+			while (redir)
+			{
+				// Eğer input redirect varsa → bu geçerli değil
+				if (redir->type == TOKEN_REDIRECT_IN || redir->type == TOKEN_HEREDOC)
+				{
+					ft_putstr_fd("minishell: ", 2);
+					ft_putstr_fd(cmd->redirects->filename, 2);
+					ft_putstr_fd(": No such file or directory", 2);
+					ft_putchar_fd('\n', 2);
+					minishell->last_exit_code = 1;
+					return (1);
+				}
+				redir = redir->next;
+			}
+			// sadece output/append redirect varsa
 			if (create_empty_redirect_files(cmd->redirects) != 0)
 			{
 				minishell->last_exit_code = 1;
 				return (1);
 			}
+			minishell->last_exit_code = 0;
+			return (1);
+		}
+		// 2. args varsa ama bunlar sadece bir operatorse (örn: |, >, >>)
+		if (minishell->tokens
+			&& minishell->tokens->value
+			&& (ft_strcmp(minishell->tokens->value, "|") == 0
+			|| ft_strcmp(minishell->tokens->value, "<") == 0
+			|| ft_strcmp(minishell->tokens->value, "<<") == 0
+			|| ft_strcmp(minishell->tokens->value, ">") == 0
+			|| ft_strcmp(minishell->tokens->value, ">>") == 0))
+		{
+			ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+			ft_putstr_fd(minishell->tokens->value, 2);
+			ft_putstr_fd("'\n", 2);
+			minishell->last_exit_code = 2;
+			return (1);
 		}
 		ft_putstr_fd("minishell: '' command not found\n", 2);
 		minishell->last_exit_code = 127;
@@ -30,6 +63,7 @@ int	empty_or_null_command(t_command *cmd, t_minishell *minishell)
 	}
 	return (0);
 }
+
 
 int	run_parent_builtin_if_needed(t_command *cmd, t_minishell *minishell)
 {

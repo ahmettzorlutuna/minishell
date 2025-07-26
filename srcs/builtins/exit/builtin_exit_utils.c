1371c243c0835b98321b1cdaf19860bfd1b35032
Ahmet Tayyip Zorlutuna <azorlutu@k1m37s02.42istanbul.com.tr>
2025-07-26 14:59:45 +0300

    exitlerin utilsleri

---
+++ srcs/builtins/exit/builtin_exit_utils.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_exit_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azorlutu <azorlutu@student.42istanbul      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 14:35:49 by azorlutu          #+#    #+#             */
+/*   Updated: 2025/07/26 14:35:50 by azorlutu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	no_args_exit(t_minishell *minishell)
+{
+	free_minishell(minishell);
+	exit(minishell->last_exit_code);
+}
+
+void	non_numeric_exit(char *arg, t_minishell *minishell)
+{
+	ft_putstr_fd("minishell: exit: ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
+	free_minishell(minishell);
+	exit(2);
+}
+
+void	too_many_args_exit(t_minishell *minishell)
+{
+	ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+	minishell->last_exit_code = 1;
+	free_minishell(minishell);
+	exit(1);
+}
+
+void	normal_exit(char *arg, t_minishell *minishell)
+{
+	long	exit_code;
+
+	exit_code = ft_atol(arg) % 256;
+	if (exit_code < 0)
+		exit_code += 256;
+	free_minishell(minishell);
+	exit(exit_code);
+}

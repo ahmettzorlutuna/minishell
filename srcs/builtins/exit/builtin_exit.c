13dc669d09578cbdf3766c6d2b39d29257a204d8
Ahmet Tayyip Zorlutuna <azorlutu@k1m37s02.42istanbul.com.tr>
2025-07-26 15:00:22 +0300

    ayırdığım fonksiyonları çağırdım

--- srcs/builtins/exit/builtin_exit.c
+++ srcs/builtins/exit/builtin_exit.c
@@ -32,32 +32,13 @@ static int	is_numeric(const char *str)
 
 int	builtin_exit(char **args, t_minishell *minishell)
 {
-	long	exit_code;
-
 	ft_putstr_fd("exit\n", 1);
 	if (!args[1])
-	{
-		free_minishell(minishell);
-		exit(minishell->last_exit_code);
-	}
+		no_args_exit(minishell);
 	if (!is_numeric(args[1]))
-	{
-		ft_putstr_fd("minishell: exit: ", 2);
-		ft_putstr_fd(args[1], 2);
-		ft_putstr_fd(": numeric argument required\n", 2);
-		free_minishell(minishell);
-		exit(2);
-	}
+		non_numeric_exit(args[1], minishell);
 	if (args[2])
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
-		minishell->last_exit_code = 1;
-		free_minishell(minishell);
-		exit(1);
-	}
-	exit_code = ft_atol(args[1]) % 256;
-	if (exit_code < 0)
-		exit_code += 256;
-	free_minishell(minishell);
-	exit(exit_code);
+		too_many_args_exit(minishell);
+	normal_exit(args[1], minishell);
+	return (0);
 }

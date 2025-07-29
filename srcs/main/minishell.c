2180bd115f8f40c8a30e9e96ba8ea0bbfb1c3c78
Ahmet Tayyip Zorlutuna <azorlutu@k1m37s02.42istanbul.com.tr>
2025-07-29 21:07:45 +0300

    pid'leri freelemek için geçici olarak temp_pids de tuttum

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -17,6 +17,7 @@ void	init_mini_data(t_minishell *minishell, char **envp)
 	minishell->input = NULL;
 	minishell->tokens = NULL;
 	minishell->command_list = NULL;
+	minishell->temp_pids = NULL;
 	minishell->last_exit_code = 0;
 	minishell->has_syntax_error = 0;
 	minishell->env_list = init_env_list(envp);
@@ -28,7 +29,7 @@ void	init_minishell(t_minishell *minishell)
 	minishell->has_syntax_error = 0;
 	add_history(minishell->input);
 	free_token_list(minishell->tokens);
-	minishell->tokens = tokenizer(minishell->input, minishell);
+	minishell->tokens = tokenizer(minishell->input);
 	expand_tokens(minishell, minishell->tokens, minishell->env_list);
 	minishell->command_list = parse_command(minishell, &minishell->tokens);
 	execute_pipeline(minishell->command_list, minishell);

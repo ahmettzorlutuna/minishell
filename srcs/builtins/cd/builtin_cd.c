5d7c866f05ce0d86b425a4e576d6bd817e971990
Ahmet Tayyip Zorlutuna <azorlutu@k1m37s02.42istanbul.com.tr>
2025-08-01 22:41:44 +0300

    env boş ise hata durumları kontrol edildi

--- srcs/builtins/cd/builtin_cd.c
+++ srcs/builtins/cd/builtin_cd.c
@@ -1,16 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azorlutu <azorlutu@student.42istanbul      +#+  +:+       +#+        */
+/*   By: ekamar <ekamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 16:33:22 by azorlutu          #+#    #+#             */
-/*   Updated: 2025/05/09 16:33:23 by azorlutu         ###   ########.fr       */
+/*   Updated: 2025/08/01 21:43:56 by ekamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static	char	*get_cd_target(char *arg, t_minishell *minishell)
+{
+	if (!arg)
+		return (get_env_value(minishell->env_list, "HOME"));
+	if (ft_strcmp(arg, "-") == 0)
+		return (get_env_value(minishell->env_list, "OLDPWD"));
+	return (arg);
+}
+
+static	int	handle_cd_null(char *arg, t_minishell *minishell)
+{
+	char	*home;
+
+	if (arg && ft_strcmp(arg, "-") == 0)
+		ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+	else
+	{
+		home = get_env_value(minishell->env_list, "HOME");
+		if (!home)
+			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+		else
+			print_cd_error(arg);
+	}
+	return (1);
+}
 
 int	builtin_cd(char **args, t_minishell *minishell)
 {
@@ -20,7 +46,9 @@ int	builtin_cd(char **args, t_minishell *minishell)
 	if (args[1] && args[2])
 		return (cd_too_many_args());
 	target_path = get_cd_target(args[1], minishell);
-	if (!target_path || chdir(target_path) != 0)
+	if (!target_path)
+		return (handle_cd_null(args[1], minishell));
+	if (chdir(target_path) != 0)
 	{
 		print_cd_error(args[1]);
 		return (1);

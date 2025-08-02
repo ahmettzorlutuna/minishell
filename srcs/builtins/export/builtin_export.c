9a9068a01eed10643f86b4c47e95f7a089f9c69c
Eylül Medine Kamar <ekamar@k1m37s04.42istanbul.com.tr>
2025-08-02 15:28:44 +0300

    art arda export da exit kod tamam

--- srcs/builtins/export/builtin_export.c
+++ srcs/builtins/export/builtin_export.c
@@ -12,31 +12,40 @@
 
 #include "../includes/minishell.h"
 
-int	builtin_export(char **args, t_minishell *minishell)
+int	handle_export_arg(char *arg, t_minishell *minishell)
 {
-	int		i;
 	char	*key;
 	char	*value;
 
+	key = NULL;
+	value = NULL;
+	if (split_key_value(arg, &key, &value) || !is_valid_key(key))
+	{
+		print_export_error(arg);
+		free(key);
+		free(value);
+		return (1);
+	}
+	set_env_and_update(minishell, key, value);
+	free(key);
+	free(value);
+	return (0);
+}
+
+int	builtin_export(char **args, t_minishell *minishell)
+{
+	int		i;
+	int		error_code;
+
+	error_code = 0;
 	if (!args[1])
 		return (handle_no_args(minishell));
 	i = 1;
 	while (args[i])
 	{
-		key = NULL;
-		value = NULL;
-		if (split_key_value(args[i], &key, &value) || !is_valid_key(key))
-		{
-			print_export_error(args[i]);
-			free(key);
-			free(value);
-			i++;
-			continue ;
-		}
-		set_env_and_update(minishell, key, value);
-		free(key);
-		free(value);
+		if (handle_export_arg(args[i], minishell))
+			error_code = 1;
 		i++;
 	}
-	return (0);
+	return (error_code);
 }
